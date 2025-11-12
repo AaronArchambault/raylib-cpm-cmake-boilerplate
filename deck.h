6d540455068765e3f5f0ce5ce523bcb5f2eccf18
BigA <robert.archambault@mymail.champlain.edu>
2025-11-12 15:27:11 -0500

    working on making it muti flies
    working on the code to have the decloraitons in a header file

--- deck.h
+++ deck.h
@@ -1,3 +1,6 @@
+#ifndef DECK_H
+#define DECK_H
+
 #include <vector>
 #include <random>
 #include <cmath>
@@ -8,23 +11,70 @@ enum cardValue {
     REVERSE, SKIP, DRAW_TWO, WILD, WILD_DRAW_FOUR
 };
 
-
-
-struct Card
-{
+struct Card {
     cardColor color;
-    cardValue value;
+    cardValue type;
 
-    bool matches(const Card& other);
-    bool operator==(const Card& other);
-    void colorChange(cardColor target);
+    bool matches(const Card& other) const {
+        return color == other.color || type == other.type ||
+               color == WILDS || other.color == WILDS;
+    }
+
+    bool operator==(const Card& other) const {
+        return color == other.color && type == other.type;
+    }
+
+    void colorChange(cardColor target) {
+        color = target;
+    }
 };
 
-class Player
-{
+class Deck {
+    std::vector<Card> cards;
+public:
+    void shuffle();
+    Card draw();
+};
+
+class Player {
 private:
-    vector<Card> hand;
+    std::vector<Card> hand;
+    bool isAI;
 
 public:
+    Player(bool ai = false) : isAI(ai) {}
 
+    bool canPlay(const Card& topCard);
+    int chooseOptimalCard(const Card& topCard, int opponentHandSize);
+    int chooseOptimalCardMultiTurn(const Card& topCard, int opponentHandSize, int turnsToAnalyze = 3);
+    Card playCard(int index);
+    void addCard(Card card);
+    int getHandSize() const;
+    const std::vector<Card>& getHand() const;
 };
+
+struct CardScore {
+    double attackingValue;
+    double defendingValue;
+    double strategicValue;
+};
+
+class LPOptimizer {
+public:
+    static CardScore calcCard(const Card& card, const Card& topCard,
+                              int handSize, int opponentHandSize);
+private:
+    static double calcAttackingValue(const Card& card, int handSize);
+    static double calcDefendingValue(const Card& card, int opponentHandSize);
+};
+
+class Game {
+    Deck deck;
+    std::vector<Player> players;
+    Card discardPile;
+public:
+    void playTurn();
+    bool checkWinner();
+};
+
+#endif
